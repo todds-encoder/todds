46c8bb83da98f21a73d335cd26d1c216591e8ecb
José Ángel Soler Ortiz <jose.angel.soler.ortiz@gmail.com>
2022-06-22 10:59:54 +0200

    Disable CLANG_TIDY by default

--- cmake/clang_tidy.cmake
+++ cmake/clang_tidy.cmake
@@ -1,6 +1,10 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 include_guard(GLOBAL)
 
-option(PNG2DDS_CLANG_TIDY "Analyze the project with clang-tidy." ON)
+option(PNG2DDS_CLANG_TIDY "Analyze the project with clang-tidy." OFF)
 
 if (PNG2DDS_CLANG_TIDY)
 	find_program(
