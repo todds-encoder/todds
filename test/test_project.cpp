e2c96d661a7c947adcf1f9b07aaa6c64093d8168
José Ángel Soler Ortiz <jose.angel.soler.ortiz@gmail.com>
2022-06-23 14:21:48 +0200

    png2dds_project testing

---
+++ test/test_project.cpp
@@ -0,0 +1,14 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+#include <png2dds/project.hpp>
+
+#include <catch2/catch.hpp>
+
+TEST_CASE("png2dds::project coherence checks", "[project]") {
+	STATIC_REQUIRE(!png2dds::project::name().empty());
+	STATIC_REQUIRE(!png2dds::project::description().empty());
+	STATIC_REQUIRE(!png2dds::project::version().empty());
+}
---
+++ test/tests.cpp
@@ -0,0 +1,5 @@
+#define CATCH_CONFIG_MAIN
+
+#include <catch2/catch.hpp>
+
+// See https://github.com/catchorg/Catch2/blob/master/docs/slow-compiles.md
