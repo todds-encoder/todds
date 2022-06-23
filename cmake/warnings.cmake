312dedb07c23363f49dda680857039838bb05c56
José Ángel Soler Ortiz <jose.angel.soler.ortiz@gmail.com>
2022-06-23 22:04:37 +0200

    Use Boost::nowide to ensure UTF-8 compatibility on Windows platforms

--- cmake/warnings.cmake
+++ cmake/warnings.cmake
@@ -98,7 +98,7 @@ elseif (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
 		/w14906      # String literal cast to 'LPWSTR'.
 		/w14928      # Illegal copy-initialization; applied more than one user-defined conversion.
 		/analyze     # Code analysis.
-		/wd6285      # Triggered by code inside of fmt.
+		/wd6239      # Triggered by code inside of fmt.
 		)
 endif ()
 
--- src/main/main.cpp
+++ src/main/main.cpp
@@ -7,9 +7,12 @@
 #include "png2dds/project.hpp"
 
 #include <argparse/argparse.hpp>
+#include <boost/nowide/args.hpp>
+#include <boost/nowide/iostream.hpp>
 #include <fmt/format.h>
 
 int main(int argc, char** argv) {
+	boost::nowide::args _(argc, argv);
 	int execution_status = EXIT_FAILURE;
 
 	auto arguments = png2dds::arguments();
@@ -17,11 +20,11 @@ int main(int argc, char** argv) {
 		arguments.parse_args(argc, argv);
 		execution_status = EXIT_SUCCESS;
 	} catch (const std::runtime_error& ex) {
-		fmt::print(stderr, "{:s}\n{:s}", ex.what(), arguments.help().str());
+		boost::nowide::cerr << fmt::format("{:s}\n{:s}", ex.what(), arguments.help().str());
 	} catch (const std::exception& ex) {
-		fmt::print(stderr, "{:s} has been terminated because of an exception: {:s}\n", png2dds::project::name(), ex.what());
+		boost::nowide::cerr << fmt::format("{:s} has been terminated because of an exception: {:s}\n", png2dds::project::name(), ex.what());
 	} catch (...) {
-		fmt::print(stderr, "{:s} has been terminated because of an unknown exception.\n", png2dds::project::name());
+		boost::nowide::cerr << fmt::format("{:s} has been terminated because of an unknown exception.\n", png2dds::project::name());
 	}
 
 	return execution_status;
