4dca16eee086cf9fb047acb3887f965b5f3a689e
José Ángel Soler Ortiz <jose.angel.soler.ortiz@gmail.com>
2022-06-23 21:13:12 +0200

    Add MSVC specific compiler options and definitions for handling UTF-8

---
+++ cmake/platform.cmake
@@ -0,0 +1,15 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+include_guard(GLOBAL)
+
+# Platform or compiler-specific defines.
+if (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+	add_compile_definitions(UNICODE _UNICODE)
+	message(STATUS "Defining UNICODE and _UNICODE for MSVC")
+	add_compile_options("/utf-8")
+	message(STATUS "Adding /utf-8 option for MSVC ")
+	add_compile_definitions(NOMINMAX WIN32_LEAN_AND_MEAN)
+	message(STATUS "Defining NOMINMAX and WIN32_LEAN_AND_MEAN for windows.h")
+endif ()
