5ceeccd57786692cb988ef47929789f1eebf2669
José Ángel Soler Ortiz <jose.angel.soler.ortiz@gmail.com>
2022-07-06 10:54:32 +0200

    Disable stack protection in debug builds to allow inspecting issues with the debugger.

--- cmake/platform.cmake
+++ cmake/platform.cmake
@@ -13,3 +13,8 @@ if (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
 	add_compile_definitions(NOMINMAX WIN32_LEAN_AND_MEAN)
 	message(STATUS "Defining NOMINMAX and WIN32_LEAN_AND_MEAN for windows.h")
 endif ()
+
+if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+	# Disable stack protection in debug builds to allow inspecting issues with the debugger.
+	add_compile_options("$<$<CONFIG:DEBUG>:-fno-stack-protector>")
+endif ()
