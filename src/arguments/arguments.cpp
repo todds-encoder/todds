e9c8260cbfdb4a28277baf38afaa3afc40c0ea82
José Ángel Soler Ortiz <jose.angel.soler.ortiz@gmail.com>
2022-07-20 12:55:00 +0200

    Improve --help formatting

--- src/arguments/arguments.cpp
+++ src/arguments/arguments.cpp
@@ -12,6 +12,7 @@
 #include <fmt/format.h>
 #include <oneapi/tbb/info.h>
 
+#include <iomanip>
 #include <ostream>
 #include <string_view>
 
@@ -48,7 +49,7 @@ constexpr std::string_view input_arg = "input";
 constexpr std::string_view input_help =
 	"Converts to DDS all PNG files inside of this folder. Can also point to a single PNG file.";
 
-constexpr std::string_view output_arg = "output_arg";
+constexpr std::string_view output_arg = "output";
 constexpr std::string_view output_help = "By default DDS files are created next to their input PNG files. If this "
 																				 "argument is provided, DDS files will be created in this folder instead.";
 
@@ -56,6 +57,20 @@ std::string get_help(const po::options_description& description) {
 	std::ostringstream ostream;
 	ostream << png2dds::project::name() << ' ' << png2dds::project::version() << "\n\n"
 					<< png2dds::project::description() << "\n\n";
+	ostream << "USAGE:\n  " << png2dds::project::name() << " [OPTIONS] " << input_arg << " [" << output_arg << ']'
+					<< "\n\n";
+
+	ostream << "ARGS:\n";
+
+	const unsigned int option_size = description.get_option_column_width();
+	ostream << "  " << input_arg;
+	std::fill_n(std::ostream_iterator<char>(ostream), option_size - 2UL - input_arg.size(), ' ');
+	ostream << input_help << '\n';
+
+	ostream << "  " << output_arg;
+	std::fill_n(std::ostream_iterator<char>(ostream), option_size - 2UL - output_arg.size(), ' ');
+	ostream << output_help << "\n\n";
+
 	description.print(ostream);
 	return std::move(ostream).str();
 }
@@ -67,7 +82,8 @@ namespace png2dds::args {
 data get(int argc, char** argv) {
 	boost::nowide::args nowide_args(argc, argv);
 
-	po::options_description description("OPTIONS");
+	// Optional arguments are defined separately to print only then when --help is used.
+	po::options_description optional_arguments("OPTIONS", 200, 50);
 	const auto max_threads = static_cast<std::size_t>(oneapi::tbb::info::default_concurrency());
 	const std::string threads_help = fmt::format(threads_help_unformatted, max_threads);
 
@@ -75,7 +91,7 @@ data get(int argc, char** argv) {
 	bool help{};
 	std::string input_str;
 	std::string output_str;
-	description.add_options()
+	optional_arguments.add_options()
 		// clang-format off
 		(level_arg.data(), po::value<unsigned int>(&arguments.level)->default_value(max_level), level_help.data())
 		(threads_arg.data(), po::value<std::size_t>(&arguments.threads)->default_value(max_threads), threads_help.c_str())
@@ -83,17 +99,24 @@ data get(int argc, char** argv) {
 		(overwrite_arg.data(), po::bool_switch(&arguments.overwrite),overwrite_help.data())
 		(flip_arg.data(), po::bool_switch(&arguments.flip),flip_help.data())
 		(time_arg.data(), po::bool_switch(&arguments.time),time_help.data())
-		(help_arg.data(), po::bool_switch(&help), help_help.data())
-		(input_arg.data(), po::value<std::string>(&input_str), input_help.data())
-		(output_arg.data(), po::value<std::string>(&output_str), output_help.data());
+		(help_arg.data(), po::bool_switch(&help), help_help.data());
+
 	// clang-format on
 
-	po::positional_options_description positional_description;
-	positional_description.add(input_arg.data(), 1);
-	positional_description.add(output_arg.data(), 1);
+	po::options_description all_arguments;
+	all_arguments.add(optional_arguments);
+	all_arguments.add_options()
+		// clang-format off
+		(input_arg.data(), po::value<std::string>(&input_str))
+		(output_arg.data(), po::value<std::string>(&output_str));
+	// clang-format on
+
+	po::positional_options_description positional_options;
+	positional_options.add(input_arg.data(), 1);
+	positional_options.add(output_arg.data(), 1);
 	try {
 		const auto parsed_options =
-			po::command_line_parser(argc, argv).options(description).positional(positional_description).run();
+			po::command_line_parser(argc, argv).options(all_arguments).positional(positional_options).run();
 		po::variables_map variables;
 		store(parsed_options, variables, true);
 		notify(variables);
@@ -104,7 +127,7 @@ data get(int argc, char** argv) {
 	}
 
 	if (help) {
-		arguments.text = get_help(description);
+		arguments.text = get_help(optional_arguments);
 	} else if (arguments.level > max_level) {
 		arguments.error = true;
 		arguments.text = fmt::format("Argument error: Unsupported encode quality level {:d}.", arguments.level);
