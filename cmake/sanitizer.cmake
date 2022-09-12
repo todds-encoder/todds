d9c00e202f63fb711e0325b45f8da261af34b374
José Ángel Soler Ortiz <jose.angel.soler.ortiz@gmail.com>
2022-09-12 22:11:54 +0200

    Use the undefined and address sanitizers when using GCC or clang

---
+++ cmake/sanitizer.cmake
@@ -0,0 +1,8 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+	add_compile_options(-fsanitize=undefined,address)
+	add_link_options(-fsanitize=undefined,address)
+endif ()
