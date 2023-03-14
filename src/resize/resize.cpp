820f645d4046ad84a072b51cf33382da5f239325
José Ángel Soler Ortiz <jose.angel.soler.ortiz@gmail.com>
2023-03-14 22:49:46 +0100

    Fix the box downscale algorithm.

--- src/resize/resize.cpp
+++ src/resize/resize.cpp
@@ -12,14 +12,27 @@ void box_downscale(const image& source, image& destination) {
 	assert((source.width() >> 1U) == destination.width());
 	assert((source.height() >> 1U) == destination.height());
 
-	const std::size_t bytes_per_row = destination.width() * image::bytes_per_pixel;
-	for (std::size_t pixel_y = 0ULL; pixel_y < destination.height(); ++pixel_y) {
-		const std::uint8_t* source_byte_0 = &source.row_start(pixel_y * 2ULL);
-		const std::uint8_t* source_byte_1 = &source.row_start(pixel_y * 2ULL + 1ULL);
-		std::uint8_t* destination_byte = &destination.row_start(pixel_y);
-		for (std::size_t byte_x = 0ULL; byte_x < bytes_per_row; ++byte_x) {
-			const std::uint16_t avg = (source_byte_0[0U] + source_byte_0[1U] + source_byte_1[0U] + source_byte_1[1U]) / 4;
-			*destination_byte = static_cast<std::uint8_t>(avg);
+	for (std::size_t destination_y = 0ULL; destination_y < destination.height(); ++destination_y) {
+		std::uint8_t* destination_byte = &destination.row_start(destination_y);
+
+		const std::size_t source_y = destination_y * 2ULL;
+		const std::uint8_t* source_byte_0 = &source.row_start(source_y);
+		const std::uint8_t* source_byte_1 = &source.row_start(source_y + 1ULL);
+
+		for (std::size_t destination_x = 0ULL; destination_x < destination.width(); ++destination_x) {
+			for (std::size_t byte = 0ULL; byte < image::bytes_per_pixel; ++byte) {
+				std::uint32_t avg = source_byte_0[0U];
+				avg += source_byte_0[image::bytes_per_pixel];
+				avg += source_byte_1[0U];
+				avg += source_byte_1[image::bytes_per_pixel];
+				avg /= 4U;
+				*destination_byte = static_cast<std::uint8_t>(avg);
+				++destination_byte;
+				++source_byte_0;
+				++source_byte_1;
+			}
+			source_byte_0 += image::bytes_per_pixel;
+			source_byte_1 += image::bytes_per_pixel;
 		}
 	}
 }
