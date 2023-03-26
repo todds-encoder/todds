d8d23fbbc3c730ae588d309f14cca3065cd87e23
Jose Angel Soler Ortiz <jose.angel.soler.ortiz@gmail.com>
2023-03-26 15:50:12 +0200

    Improve pipeline filter generation code.

--- src/pipeline/get_filters_from_settings.cpp
+++ src/pipeline/get_filters_from_settings.cpp
@@ -14,31 +14,18 @@
 
 namespace todds::pipeline::impl {
 
-oneapi::tbb::filter<void, void> pipeline_filters(const input& input_data, std::atomic<std::size_t>& counter,
-	std::atomic<bool>& force_finish, oneapi::tbb::concurrent_queue<std::string>& error_log,
-	vector<impl::file_data>& files_data) {
+oneapi::tbb::filter<void, std::unique_ptr<mipmap_image>> png_decoding_filters(const input& input_data,
+	std::atomic<std::size_t>& counter, std::atomic<bool>& force_finish,
+	oneapi::tbb::concurrent_queue<std::string>& error_log, vector<impl::file_data>& files_data) {
 	return // Load PNG files into memory, one by one.
 		impl::load_png_filter(input_data.paths, counter, force_finish, error_log) &
 		// Decode a PNG file into pixels.
-		impl::decode_png_filter(files_data, input_data.paths, input_data.vflip, input_data.mipmaps, error_log) &
-		// Convert images into pixel block images. The pixels of these images are rearranged into 4x4 blocks,
-		// ready for the DDS encoding stage.
-		impl::pixel_blocks_filter() &
-		// Encode pixel block images as DDS files.
-		impl::encode_dds_filter(files_data, input_data.format, input_data.quality) &
-		// Save DDS files back into the file system, one by one.
-		impl::save_dds_filter(files_data, input_data.paths);
+		impl::decode_png_filter(files_data, input_data.paths, input_data.vflip, input_data.mipmaps, error_log);
 }
 
-oneapi::tbb::filter<void, void> pipeline_filters_mipmaps(const input& input_data, std::atomic<std::size_t>& counter,
-	std::atomic<bool>& force_finish, oneapi::tbb::concurrent_queue<std::string>& error_log,
-	vector<impl::file_data>& files_data) {
-	return // Load PNG files into memory, one by one.
-		impl::load_png_filter(input_data.paths, counter, force_finish, error_log) &
-		// Decode a PNG file into pixels.
-		impl::decode_png_filter(files_data, input_data.paths, input_data.vflip, input_data.mipmaps, error_log) &
-		// Generate all mipmaps.
-		impl::generate_mipmaps_filter(input_data.mipmap_filter) &
+oneapi::tbb::filter<std::unique_ptr<mipmap_image>, void> dds_encoding_filters(
+	const input& input_data, vector<impl::file_data>& files_data) {
+	return
 		// Convert images into pixel block images. The pixels of these images are rearranged into 4x4 blocks,
 		// ready for the DDS encoding stage.
 		impl::pixel_blocks_filter() &
@@ -51,8 +38,9 @@ oneapi::tbb::filter<void, void> pipeline_filters_mipmaps(const input& input_data
 oneapi::tbb::filter<void, void> get_filters_from_settings(const input& input_data, std::atomic<std::size_t>& counter,
 	std::atomic<bool>& force_finish, oneapi::tbb::concurrent_queue<std::string>& error_log,
 	vector<impl::file_data>& files_data) {
-	const auto gen_func = input_data.mipmaps ? pipeline_filters_mipmaps : pipeline_filters;
-	return gen_func(input_data, counter, force_finish, error_log, files_data);
+	auto prepare_image = png_decoding_filters(input_data, counter, force_finish, error_log, files_data);
+	if (input_data.mipmaps) { prepare_image &= impl::generate_mipmaps_filter(input_data.mipmap_filter); }
+	return prepare_image & dds_encoding_filters(input_data, files_data);
 }
 
 } // namespace todds::pipeline::impl
